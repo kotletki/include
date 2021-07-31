5bdb3564c9d2102b330064b467a0212c2c431139
kotletki <86575613+kotletki@users.noreply.github.com>
2021-07-31 13:41:37 -0400

    Update img.h.php

--- img.h.php
+++ img.h.php
@@ -1,66 +1,66 @@
 <?php
 
-	function img($source, $width = 200, $height = 150, $filename = "", $watermark = "", $wm_x = 10, $wm_y = 10) {
+function img($source, $width = 200, $height = 150, $filename = "", $watermark = "", $wm_x = 10, $wm_y = 10) {
 
-        // .GIF .JPG .PNG
+    // .GIF .JPG .PNG
 
-        // $filename > WRITE IMAGE TO FILE
-        // IF NOT SET > OUTPUT TO SCREEN
+    // $filename > WRITE IMAGE TO FILE
+    // IF NOT SET > OUTPUT TO SCREEN
 
-		if (! isset($width)) {$width = 200;}
-		if (! isset($height)) {$height = 150;}
+    if (! isset($width)) {$width = 200;}
+    if (! isset($height)) {$height = 150;}
 
-		$src_img = imagecreatefromjpeg($source);
+    $src_img = imagecreatefromjpeg($source);
 
-	    // CALCULATE PROPORTION
+    // CALCULATE PROPORTION
 
-		$src_w = imagesx($src_img);
-		$src_h = imagesy($src_img);
+    $src_w = imagesx($src_img);
+    $src_h = imagesy($src_img);
 
-        $scale_on = ($src_w >= $src_h) ? "x" : "y";
+    $scale_on = ($src_w >= $src_h) ? "x" : "y";
 
-		if ("x" == $scale_on) {
-			if ($src_w > $width) {
-				$dst_w = $width;
-				$ratio = $dst_w / $src_w;
-				$dst_h = round($src_h * $ratio);
-			} else {
-				$dst_w = $src_w;
-				$dst_h = $src_h;
-			}
-		} else {
-			if ($src_h > $height) {
-				$dst_h = $height;
-				$ratio = $dst_h / $src_h;
-				$dst_w = round($src_w * $ratio);
-			} else {
-				$dst_w = $src_w;
-				$dst_h = $src_h;
-			}
-		}
+    if ("x" == $scale_on) {
+        if ($src_w > $width) {
+            $dst_w = $width;
+            $ratio = $dst_w / $src_w;
+            $dst_h = round($src_h * $ratio);
+        } else {
+            $dst_w = $src_w;
+            $dst_h = $src_h;
+        }
+    } else {
+        if ($src_h > $height) {
+            $dst_h = $height;
+            $ratio = $dst_h / $src_h;
+            $dst_w = round($src_w * $ratio);
+        } else {
+            $dst_w = $src_w;
+            $dst_h = $src_h;
+        }
+    }
 
-	    // CREATE COPY
+    // CREATE COPY
 
-		$dst_img = imagecreatetruecolor($dst_w, $dst_h);
-		imagecopyresampled($dst_img, $src_img, 0, 0, 0, 0, $dst_w, $dst_h, $src_w, $src_h);
-	
-	    // WATERMARK
+    $dst_img = imagecreatetruecolor($dst_w, $dst_h);
+    imagecopyresampled($dst_img, $src_img, 0, 0, 0, 0, $dst_w, $dst_h, $src_w, $src_h);
 
-		if ($watermark) {
-			$stamp = imagecreatefrompng($watermark);
-			$sx = imagesx($stamp);
-			$sy = imagesy($stamp);
-			imagecopy($dst_img, $stamp, imagesx($dst_img) - $sx - $wm_x, imagesy($dst_img) - $sy - $wm_y, 0, 0, imagesx($stamp), imagesy($stamp));
-		}
+    // WATERMARK
 
-        // OUTPUT
+    if ($watermark) {
+        $stamp = imagecreatefrompng($watermark);
+        $sx = imagesx($stamp);
+        $sy = imagesy($stamp);
+        imagecopy($dst_img, $stamp, imagesx($dst_img) - $sx - $wm_x, imagesy($dst_img) - $sy - $wm_y, 0, 0, imagesx($stamp), imagesy($stamp));
+    }
 
-		if ($filename) {
-			imagejpeg($dst_img, $filename, 100);
-		} else {
-			header("Content-Type:image/jpeg");
-			imagejpeg($dst_img);
-		}
+    // OUTPUT
 
-		imagedestroy($dst_img);
-	}
+    if ($filename) {
+        imagejpeg($dst_img, $filename, 100);
+    } else {
+        header("Content-Type:image/jpeg");
+        imagejpeg($dst_img);
+    }
+
+    imagedestroy($dst_img);
+}
